a13c9ac194c2688634ddeef43df81adca4445455
alexp <alexandru.pavel.82@gmail.com>
2016-06-19 15:16:27 +0300

    add outh2 security using the spring security

---
+++ src/main/java/com/keba/rest/poll/OAuth2ResourceServerConfig.java
@@ -0,0 +1,27 @@
+package com.keba.rest.poll;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+/**
+ * Created by alexp on 19/06/16.
+ */
+@Configuration
+@EnableResourceServer
+public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId("QuickPoll_Resources");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers().antMatchers("/oauth2/v3/polls/**")
+                .and()
+                .authorizeRequests().antMatchers("/oauth2/v3/polls/**").authenticated();
+    }
+}
--- src/main/java/com/keba/rest/poll/SecurityConfig.java
+++ src/main/java/com/keba/rest/poll/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.keba.rest.poll;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,5 +45,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable();
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
